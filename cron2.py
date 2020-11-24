d24ab1a195ef565b218299ce999d6991706366ac
Rowan-W-Fitch <rfitch@oxy.edu>
2020-11-24 11:59:29 -0800

    cron2 update

--- cron2.py
+++ cron2.py
@@ -2,3 +2,6 @@ import requests
 
 req = requests.get("https://optimalstoke.herokuapp.com/")
 print("req: ", req)
+
+req2 = requests.get("https://optimal-stoke.herokuapp.com/")
+print("req2: ", req2)
